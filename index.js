d69fcb3a98b6f5fc4cc6757a9fe0d5e50d37b03d
Emmanuel537-cmd <emmanuelchikobvu3@gmail.com>
2025-06-16 20:41:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
     // You can also use a QR code generator to display this QR in a browser
 });
+
+
+js
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode');
+const express = require('express');
+const app = express();
+
+const client = new Client();
+let qrCodeData = '';
+
+client.on('qr', (qr) => {
+    qrCodeData = qr;
+});
+
+client.initialize();
+
+app.get('/scanner', async (req, res) => {
+    if (qrCodeData) {
+        const qrImage = await qrcode.toDataURL(qrCodeData);
+        res.send(`<img src="${qrImage}" style="width:300px;"/>`);
+    } else {
+        res.send('Loading QR...');
+    }
+});
+
+app.listen(3000, () => console.log('QR Scanner running on port 3000'));
